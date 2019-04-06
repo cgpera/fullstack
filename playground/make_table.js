c545c2cf121de7eda43712a91850980c7baa7036
Claudio Pera <cgpera@gmail.com>
2019-04-05 22:06:51 -0300

    tabla de array de senadores en js

--- playground/make_table.js
+++ playground/make_table.js
@@ -8,15 +8,19 @@ var encab = '<table id="senate-data">' +
 
 var pie = '</tbody></table>'
 
-
-// dos formas de reemplazo de variables
 //var cuerpo_tabla = data.results[0].members.map(function (array, index) {return ('<tr><td>' + (index+1) + '</td> ' + '<td><a href="' + array.url + '" target="_blank">' + array.first_name + ' ' + (array.middle_name || '') + ' ' + array.last_name + '</a></td><td>' + array.party + '</td><td>' + array.state + '</td><td>' + array.seniority + '</td><td>' + array.votes_with_party_pct + ' % </td></tr>')})
 //var cuerpo_tabla = data.results[0].members.map(function (array, index) { return('<tr><td>' + (index + 1) + '</td> ' + '<td>' + `<a href= ${array.url} target='_blank'>` + array.first_name + ' ' + (array.middle_name || '') + ' ' + array.last_name + '</a></td><td>' + array.party + '</td><td>' + array.state + '</td><td>' + array.seniority + '</td><td>' + array.votes_with_party_pct + ' % </td></tr>') })
-
+//document.write(encab+cuerpo_tabla)
 document.getElementById("senate-data").innerHTML = encab
 document.getElementById("senate-data").innerHTML += '<tbody>'
 
 // meto en el html del id senate-data las filas de cada senador
-// el reemplazo de variables con ${var} es mérito del profesor
-data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += ('<tr><td>' + (index + 1) + '</td> ' + '<td>' + `<a href= ${array.url} target='_blank'>` + array.first_name + ' ' + (array.middle_name || '') + ' ' + array.last_name + '</a></td><td>' + array.party + '</td><td>' + array.state + '</td><td>' + array.seniority + '</td><td>' + array.votes_with_party_pct + ' % </td></tr>') })
+//, el reemplazo de variables con ${var} es mérito del profesor
+// el sig método es con reemplazo
+//data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += ('<tr><td>' + (index + 1) + '</td> ' + '<td>' + `<a href= ${array.url} target='_blank'>` + array.first_name + ' ' + (array.middle_name || '') + ' ' + array.last_name + '</a></td><td>' + array.party + '</td><td>' + array.state + '</td><td>' + array.seniority + '</td><td>' + array.votes_with_party_pct + ' % </td></tr>') })
+
+//data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += (`<tr><td>  (${index} + 1) </td><td> <a href= ${array.url} target='_blank'> ${array.first_name}  (${array.middle_name} || '')  ${array.last_name} `)}
+// + '</a></td><td>' + ${array.party} + '</td><td>' + ${array.state} + '</td><td>' + ${array.seniority} + '</td><td>' + ${array.votes_with_party_pct} + ' % </td></tr>')`)}
 document.getElementById("senate-data").innerHTML += pie
+// finalmente haciendo reemplazos de strings:
+data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += `<tr><td> ${index + 1} </td> <td> <a href= ${array.url} target='_blank'> ${array.first_name} ${array.middle_name || ''} ${array.last_name} </a></td><td> ${array.party} </td><td> ${array.state} </td><td> ${array.seniority} </td><td> ${array.votes_with_party_pct} % </td></tr>`})
