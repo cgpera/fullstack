e67f8f435e10d9181ae28f9303e1864ac6cadca4
Claudio Pera <cgpera@gmail.com>
2019-04-10 21:58:05 -0300

    filtros

--- playground/make_table_house.js
+++ playground/make_table_house.js
@@ -1,6 +1,6 @@
 var encab = '<table id="house-data">' +
-  '<thead>' + '<tr>' + '<th scope="col">Orden</th>' +
-  '<th scope="col" colspan="1">Name</th>' +
+  '<thead>' + '<tr>' + '<th scope="col">Order</th>' +
+  '<th scope="col">  Name</th>' +
   '<th scope="col">Party</th>' +
   '<th scope="col">State</th>' +
   '<th scope="col">Seniority</th>' +
@@ -10,6 +10,5 @@ var pie = '</tbody></table>'
 
 document.getElementById("house-data").innerHTML = encab
 document.getElementById("house-data").innerHTML += '<tbody>'
-document.getElementById("house-data").innerHTML += pie
-
 data.results[0].members.map(function (array, index) { document.getElementById("house-data").innerHTML += `<tr><td> ${index + 1} </td> <td> <a href= ${array.url} target='_blank'> ${array.first_name} ${array.middle_name || ''} ${array.last_name} </a></td><td> ${array.party} </td><td> ${array.state} </td><td> ${array.seniority} </td><td> ${array.votes_with_party_pct} % </td></tr>`})
+document.getElementById("house-data").innerHTML += pie
--- playground/make_table_senate.js
+++ playground/make_table_senate.js
@@ -1,5 +1,5 @@
 var encab = '<table id="senate-data">' +
-  '<thead>' + '<tr>' + '<th scope="col">Orden</th>' +
+  '<thead>' + '<tr>' + '<th scope="col">Order</th>' +
   '<th scope="col" colspan="1">Name</th>' +
   '<th scope="col">Party</th>' +
   '<th scope="col">State</th>' +
@@ -21,6 +21,7 @@ document.getElementById("senate-data").innerHTML += '<tbody>'
 
 //data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += (`<tr><td>  (${index} + 1) </td><td> <a href= ${array.url} target='_blank'> ${array.first_name}  (${array.middle_name} || '')  ${array.last_name} `)}
 // + '</a></td><td>' + ${array.party} + '</td><td>' + ${array.state} + '</td><td>' + ${array.seniority} + '</td><td>' + ${array.votes_with_party_pct} + ' % </td></tr>')`)}
-document.getElementById("senate-data").innerHTML += pie
+
 // finalmente haciendo reemplazos de strings:
 data.results[0].members.map(function (array, index) { document.getElementById("senate-data").innerHTML += `<tr><td> ${index + 1} </td> <td> <a href= ${array.url} target='_blank'> ${array.first_name} ${array.middle_name || ''} ${array.last_name} </a></td><td> ${array.party} </td><td> ${array.state} </td><td> ${array.seniority} </td><td> ${array.votes_with_party_pct} % </td></tr>`})
+document.getElementById("senate-data").innerHTML += pie
